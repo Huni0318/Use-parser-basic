88445519d0d54e59c5382639fe6ab5625c59080a
Huni0318 <zlatkd12@naver.com>
2021-12-24 11:06:09 +0900

    .

--- 04. cnn basic/train.py
+++ 04. cnn basic/train.py
@@ -4,9 +4,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from model import ConvolutionalClassifier
-from trainer import Trainer
-from data_loader import get_loaders
+from mnist_classification.data_loader import get_loaders
+from mnist_classification.trainer import Trainer
+
+from mnist_classification.models.fc_model import FullyConnectedClassifier
+from mnist_classification.models.cnn_model import ConvolutionalClassifier
 
 def define_argparser():
     p = argparse.ArgumentParser()
@@ -20,9 +22,22 @@ def define_argparser():
     p.add_argument('--n_epochs', type=int, default=20)
     p.add_argument('--verbose', type=int, default=2)
 
+    p.add_argument('--model', type=str, default='fc')
+
     config = p.parse_args()
 
     return config
+
+
+def get_model(config):
+    if config.model == 'fc':
+        model = FullyConnectedClassifier(28**2, 10)
+    elif config.model == 'cnn':
+        model = ConvolutionalClassifier(10)
+    else:
+        raise NotImplementedError('You need to specify model name.')
+
+    return model
 
 
 def main(config):
@@ -35,9 +50,14 @@ def main(config):
     print("Valid:", len(valid_loader.dataset))
     print("Test:", len(test_loader.dataset))
 
-    model = ConvolutionalClassifier(10).to(device)
+    model = get_model(config).to(device)
     optimizer = optim.Adam(model.parameters())
     crit = nn.NLLLoss()
+
+    if config.verbose >= 2:
+        print(model)
+        print(optimizer)
+        print(crit)
 
     trainer = Trainer(config)
     trainer.train(model, crit, optimizer, train_loader, valid_loader)
